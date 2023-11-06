fa058aa6b0c29c4ba341436043fd542d542247cc
Adesh Lad <adesh.lad@cctech.co.in>
2023-11-06 17:39:54 +0530

    added Reader.h, Reader.cpp

---
+++ headers/Reader.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include "Triangulation.h"
+#include "headers/TypeDef.h"
+
+class Reader
+{
+public:
+    Reader();
+    ~Reader();
+
+    void readOBJ(std::string filePath, Triangulation &triangulation);
+    void readVertex(std::string line, PointList &points);
+    void readNormal(std::string line, PointList &normals);
+    void readFacet(std::string line, PointList &points, PointList &normals, Triangulation &triangulation);
+
+private:
+};
