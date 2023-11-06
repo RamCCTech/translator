49bc610060f673b0c3943f0f4c3ec0d7b6645568
Adesh Lad <adesh.lad@cctech.co.in>
2023-11-06 19:03:40 +0530

    added main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
+#include <iostream>
 #include"../headers/Triangulation.h"
 #include"../headers/Reader.h"
-
+using namespace std;
+ 
 int main()
 {
     Triangulation triangulation;
-
-    Reader read;
-    read.readSTL("../resources/cubeModel.stl", triangulation);
-
+ 
+    // Reader read();
+   // read.("../resources/cubeModel.stl", triangulation);
+ 
+    cout << "           welcome to translator application       " << endl;
+    cout << "--------------------------------------------------------" << endl;
+    cout << "1. OBJ to STL file format" << endl;
+    cout << "2. STL to OBJ file format" << endl;
+    int option{};
+    cin>> option;
+    switch (option)
+    {
+        case 1:
+        {
+            Reader reader;
+            reader.readOBJ("translator\\resources\\cube.obj",triangulation);
+ 
+            Writer
+           
+           
+        }
+        case 2:
+        {
+ 
+        }
+    }
     return 0;
 }
