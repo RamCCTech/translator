1aeb8a7e83bebc64465138867ad3841480180b45
kunalMore23 <kunal.more@cctech.co.in>
2023-11-06 17:03:44 +0530

    added back triangulation files

---
+++ src/Triangulation.cpp
@@ -0,0 +1,26 @@
+#include"../headers/Triangulation.h"
+ 
+Triangulation::Triangulation()
+{
+ 
+}
+ 
+Triangulation::~Triangulation()
+{
+ 
+}
+ 
+TriangleList Triangulation::triangles()
+{
+    return mTriangles;
+}
+ 
+PointList Triangulation::points()
+{
+    return mPoints;
+}
+ 
+PointList Triangulation::normals()
+{
+    return mNormals;
+}
