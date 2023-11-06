33b4c786a569f3b8d1322571cd4846be00bfbbcc
adesh6601 <149320733+adesh6601@users.noreply.github.com>
2023-11-06 19:29:34 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
-#include "headers/Triangulation.h"
-#include "headers/Reader.h"
-
+#include <iostream>
+#include"./headers/Triangulation.h"
+#include"./headers/Reader.h"
+#include"./headers/Writer.h"
+using namespace std;
+ 
 int main()
 {
     Triangulation triangulation;
-
-    Reader read;
-    read.readSTL("resources/cubeModel.stl", triangulation);
-
+ 
+    // Reader read();
+   // read.("../resources/cubeModel.stl", triangulation);
+ 
+    cout << "           welcome to translator application       " << endl;
+    cout << "--------------------------------------------------------" << endl;
+    cout << "1. OBJ to STL file format" << endl;
+    cout << "2. STL to OBJ file format" << endl;
+    int option{};
+    cin>> option;
+    switch (option)
+    {
+        case 1:
+        {
+            Reader reader;
+            reader.readOBJ("resources\\cube.obj",triangulation);
+ 
+            Writer writer;
+            writer.writeObjToStl("output\\outputSTL.stl",triangulation);
+                   
+        }
+        case 2:
+        {
+ 
+        }
+    }
     return 0;
 }
